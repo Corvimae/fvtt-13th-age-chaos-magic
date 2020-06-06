b26fdb7b86da0cb7885868ad202baa4fff084d3b
Corvimae <may@maybreak.com>
2020-06-06 13:47:12 -0500

    Fix init process.

--- modules/chaos.js
+++ modules/chaos.js
@@ -1,12 +1,14 @@
-game.chaosMage = game.chaosMage || {}
+Hooks.once('init', () => {
+  game.chaosMage = game.chaosMage || {}
 
-game.chaosMage.resetMacros = () => {
-  game.user.assignHotbarMacro(
-    game.macros.find(x => x.name === "Chaos Magic"),
-    1
-  );
+  game.chaosMage.resetMacros = () => {
+    game.user.assignHotbarMacro(
+      game.macros.find(x => x.name === "Chaos Magic"),
+      1
+    );
 
-  for(let i = 2; i <= 10; i += 1) {
-    game.user.assignHotbarMacro(null, i);
+    for(let i = 2; i <= 10; i += 1) {
+      game.user.assignHotbarMacro(null, i);
+    }
   }
-}
+});
