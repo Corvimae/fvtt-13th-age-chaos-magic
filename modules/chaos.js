7726cd830ad36d5c3f8b6c9c4556e06ce2a2053a
Corvimae <may@maybreak.com>
2020-06-06 13:42:54 -0500

    Initial commit.

---
+++ modules/chaos.js
@@ -0,0 +1,12 @@
+game.chaosMage = game.chaosMage || {}
+
+game.chaosMage.resetMacros = () => {
+  game.user.assignHotbarMacro(
+    game.macros.find(x => x.name === "Chaos Magic"),
+    1
+  );
+
+  for(let i = 2; i <= 10; i += 1) {
+    game.user.assignHotbarMacro(null, i);
+  }
+}
