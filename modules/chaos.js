c85b4cf9b48cc3466e673e9a204c24df37376465
Corvimae <may@maybreak.com>
2020-06-06 16:12:32 -0500

    Add helper.

--- modules/chaos.js
+++ modules/chaos.js
@@ -1,6 +1,35 @@
 Hooks.once('init', () => {
   game.chaosMage = game.chaosMage || {}
 
+  game.chaosMage.displayPower = (name, img, data) => {
+    const currentSpeaker = ChatMessage.getSpeaker();
+
+    if (!currentSpeaker?.actor) {
+      ui.notifications.error('No token is currently selected.');
+    }
+
+    const currentActor = game.actors.get(currentSpeaker.actor);
+
+    var templateData = {
+      actor: currentActor,
+      item: { 
+        name,
+        img
+      },
+      data,
+    };
+
+    let template = 'systems/archmage/templates/chat/power-card.html';
+    
+    renderTemplate(template, templateData).then(content => {
+      ChatMessage.create({
+        user: game.user._id,
+        speaker: ChatMessage.getSpeaker(),
+        content: content
+      });
+    });
+  }
+
   game.chaosMage.resetMacros = () => {
     game.user.assignHotbarMacro(
       game.macros.find(x => x.name === "Chaos Magic"),
